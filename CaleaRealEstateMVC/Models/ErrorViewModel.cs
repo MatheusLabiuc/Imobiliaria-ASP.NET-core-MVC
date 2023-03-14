b7b123187eb14fdb3badff27e8421b82c99bd115
Matheus Labiuc <Matheuslabiuc@hotmail.com>
2023-03-14 20:51:19 -0300

    Add project files.

---
+++ CaleaRealEstateMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CaleaRealEstateMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
